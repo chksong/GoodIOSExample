d0062672f407082485c89ed25fed24a24d772955
chksong <chksong@qq.com>
2015-05-05 10:48:23 +0800

    添加查看 github 代码的窗口

---
+++ GoodIOSExample/BrowseCodeViewController.m
@@ -0,0 +1,37 @@
+//
+//  BrowseCodeViewController.m
+//  GoodIOSExample
+//
+//  Created by chksong on 15-5-5.
+//  Copyright (c) 2015年 chksong company. All rights reserved.
+//
+
+#import "BrowseCodeViewController.h"
+
+@interface BrowseCodeViewController ()
+
+@end
+
+@implementation BrowseCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
