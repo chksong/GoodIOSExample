dd2ebe8537f6815cdb63d7d084d5095fe655405f
chksong <chksong@qq.com>
2015-05-05 10:51:40 +0800

    Revert "删除无用的文件"
    This reverts commit d117a4d5aaa5607c360f5bae7330e5858ed04416.

---
+++ GoodIOSExample/BrowseCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseCodeViewController.h
+//  GoodIOSExample
+//
+//  Created by chksong on 15-5-5.
+//  Copyright (c) 2015年 chksong company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseCodeViewController : UIViewController
+
+@end
