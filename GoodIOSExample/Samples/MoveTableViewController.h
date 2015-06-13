6050b2c07c3a0150177eebe3e96dcfa185b8bd94
chksong <chksong@qq.com>
2015-06-14 06:01:17 +0800

    测试 move table的架子 要搭建

---
+++ GoodIOSExample/Samples/MoveTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoveTableViewController.h
+//  GoodIOSExample
+//
+//  Created by chksong on 15/6/14.
+//  Copyright (c) 2015年 chksong company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoveTableViewController : UIViewController
+
+@end
