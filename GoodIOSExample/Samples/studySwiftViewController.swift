a2546df416f1669b22eb54dad2920e98cf4d2bef
chksong <chksong@qq.com>
2015-07-17 16:58:41 +0800

    属性字符串 NSMutableAttributedString

--- GoodIOSExample/Samples/studySwiftViewController.swift
+++ GoodIOSExample/Samples/studySwiftViewController.swift
@@ -31,7 +31,6 @@ class studySwiftViewController: UIViewController {
         
         imageView.contentMode = .ScaleAspectFit
         imageView.image = image
-    
         
         imageView.center = view.center ;
         viewContainer.addSubview(imageView)
@@ -42,7 +41,50 @@ class studySwiftViewController: UIViewController {
         let label1 = UILabel(frame: CGRect(x: 20, y: 60, width: 100, height: 23))
         label1.text = "IOS Programming Cookbook"
         label1.font = UIFont.boldSystemFontOfSize(14) ;
+//        label1.numberOfLines = 5
+//        label1.lineBreakMode = .ByWordWrapping   //空格自动换行
+        
+        label1.sizeToFit() //自动拉伸区域
+        label1.adjustsFontSizeToFitWidth = true   //自动调整字体。适应到区域
+        
+        let label2 = UILabel(frame: CGRect(x: 20, y: 100, width: 100, height: 23))
+        label2.backgroundColor = UIColor.clearColor()
+        label2.attributedText = attributedText()
+        label2.sizeToFit()
+        
+     
         viewContainer.addSubview(label1)
+        viewContainer.addSubview(label2)
+    }
+    
+    
+    //属性 字符串。      
+    func attributedText() -> NSAttributedString {
+        let string = "iOS SDK" as NSString
+        let result = NSMutableAttributedString(string: string as String)
+        
+        
+        let attributesForFirstWord = [
+            NSFontAttributeName:UIFont.boldSystemFontOfSize(60),
+            NSForegroundColorAttributeName :UIColor.redColor(),
+            NSBackgroundColorAttributeName : UIColor.blackColor(),
+        ]
+        
+        let shaddow = NSShadow()
+        shaddow.shadowColor = UIColor.darkGrayColor()
+        shaddow.shadowOffset = CGSize(width: 3, height: 3)
+        
+        let attributeForSencond = [
+            NSFontAttributeName :UIFont.boldSystemFontOfSize(60),
+            NSForegroundColorAttributeName :UIColor.whiteColor(),
+            NSBackgroundColorAttributeName :UIColor.redColor(),
+            NSShadowAttributeName : shaddow,
+        ]
+        
+        result.setAttributes(attributesForFirstWord, range: string.rangeOfString("iOS"))
+        result.setAttributes(attributeForSencond, range: string.rangeOfString("SDK"))
+        
+        return NSAttributedString(attributedString: result)
     }
     
     
